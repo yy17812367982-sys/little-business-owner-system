24b4b65f45c50140243d3d6882485701e3f07745
yy17812367982-sys <yy17812367982@gmail.com>
2026-01-14 15:10:16 +0800

    Update pythonapp.py

--- pythonapp.py
+++ pythonapp.py
@@ -1029,7 +1029,11 @@ if "clear_top_ask_ai" not in st.session_state:
 if "top_last_status" not in st.session_state:
     st.session_state.top_last_status = ""
 
-with st.expander(t("问 AI（入口）", "Ask AI (Top Entry)"), expanded=False):
+with st.expander(
+    t("问 AI（入口）", "<span class='askai-text'>ASK AI</span>"),
+    expanded=False
+):
+
     if st.session_state.clear_top_ask_ai:
         st.session_state.clear_top_ask_ai = False
         st.session_state["top_ask_ai"] = ""
